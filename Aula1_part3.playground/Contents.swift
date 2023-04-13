b4469d34dfbd4439b967dbabff0d58ab80049c95
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-13 13:15:27 -0300

    Aula1_part3
    Paradigma Orientado a Objetos

---
+++ Aula1_part3.playground/Contents.swift
@@ -0,0 +1,41 @@
+// Paradigma Orientado a Objetos
+
+// A nossa classe funciona como um molde que possui atributos e métodos. Nas classes precisamos ter o inicializador que inicializa de fato todos os atributos.
+// Mas a nossa classe é só um molde, precisamos criar objetos que instanciam essa classe para utlizarmos atributos e métodos. Então, imagine que a classe é a nossa receita de bolo e a partir dessa receita podemos fazer diversos bolos diferentes.
+
+// O que é um paradigma?
+// Ao falar sobre classes e structs estamos falando sobre um paradigma orientado a objetos, mas o o que é um paradigma de programação?
+
+// Paradigma de programação são estilos, modos, jeitos que você pode escrever e estruturar seu código para resolver algum problema. É uma questão de ideias, boas práticas, que você pode seguir.
+
+/* Paradigma orientado a objetos
+ - Baseia-se no conceito de classes e objetos;
+ - Classes possuem atributos e métodos;
+ - No Swift, você pode usar struct e classes.*/
+
+
+/* Como representar coisas do mundo real em código?
+ O paradigma orientado a objetos pega as coisas do mundo real, abstraimos essas ideias e as tranformamos em código.
+ - Exemplo prático
+ Imagine que você está construindo uma aplicação que precise realizar o cadastro de estudantes para uma maratona de programação. Você pode ter uma classe chamada Estudante que contenha diferentes atributos como:
+ 
+ - nome: String
+ - sobrenome: String
+ - instituicaoEnsino: String
+ - pontuacao: Double = 0
+ 
+ E também métodos como:
+ 
+ - marcaPonto() -> Void
+ - exibeResultado() -> Void
+ 
+ No caso, coloquei Void que significa que a função não tem nenhum retorno.
+ 
+ Imagine as informações acima como sendo um diagrama de classes. Não é de fato o código em determinada linguagem, mas é como a gente representaria uma classe Estudante.
+ 
+ Estudante 01
+ nome: "Natasha"
+
+ sobrenome: "Brandão"
+
+ instituicaoEnsino: "UNESA" */
