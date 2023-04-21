7d8782f38b2bd852b59ba95f6b34ae529333a0f5
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-21 14:19:00 -0300

    Aula4_part4

--- Aula4_part4.playground/Contents.swift
+++ Aula4_part4.playground/Contents.swift
@@ -1,3 +1,44 @@
-import UIKit
+// ENUMERAÇÕES -> esse tipo de dado perpite que a gente defina casos concretos.
+// As enumerações são usadas também com o switch por conseguirmos abordar diferentes casos. Podemos realizar essas enumerações para qualquer tipo de caso.
 
-var greeting = "Hello, playground"
+enum FormaDePagamento {
+    case pix
+    case boleto
+    case saldoEmConta
+}
+
+class Conta {
+    var saldo = 0.0
+    var nome: String
+
+    func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+    
+    func pagamentoCartao(_ tipoPagamento: FormaDePagamento) {
+        switch tipoPagamento {
+        case .pix: print("O pagamento será efetuado por pix")
+        case .boleto: print("O pagamento será efetuado por boleto")
+        case .saldoEmConta: print("O pagamento será efetuado por saldo em conta")
+        }
+    }
+
+    init(nome: String) {
+        self.nome = nome
+    }
+}
+
+var contaNatasha = Conta(nome: "Natasha")
+contaNatasha.pagamentoCartao(.pix)
+
+
+enum Mes {
+    case janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro
+}
+
+var janeiro = Mes.janeiro
+var fevereiro: Mes = .fevereiro
